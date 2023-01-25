88720df5d5800e0fdd72a7652c8507267e60f87e
mayurtechs <maddydiwan@gmail.com>
2023-01-25 13:32:48 +0530

    Hi this is second change in HomePage file

--- testpages.cs
+++ testpages.cs
@@ -3,6 +3,14 @@ namespace tetpages.HomePage
 public class HomePage
 {
 By emailid = By.id('email');
+
+        public void EnterEmail()
+        {
+
+            driver.findelement(emailid).SendKeys("maddyd@gmail.com");
+        }
+
+
 }
 
 }
